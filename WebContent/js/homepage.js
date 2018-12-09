585b958cc2dd9f0c460f873c9ed16e26e14c3e5a
何少文 <985934131@qq.com>
2018-12-09 14:03:03 +0800

    用户数据表前端功能完成
    1. 实现点击修改键后的ui改变。
    2. 实现点击取消后用户数据还原
    3. 实现点击修改然后保存后表中数据更新。
    4. 现在就差在数据库中更新用户信息了！

--- WebContent/js/homepage.js
+++ WebContent/js/homepage.js
@@ -89,14 +89,112 @@ function deleteUserFromTable(self){
 	var user = {};
 	//记住获取jquery对象的子节点是这样的！！！
 	user.id = $tableBodyj.children("td").eq(0).text();
-	user.username = $tableBodyj.children("td").eq(1).text()
+	user.username = $tableBodyj.children("td").eq(1).text();
 	//删除数据库信息
 	request("DELETE", "http://localhost:8080/suit/useroperate/operate",user,true,
-			function(){
-				alert("删除用户 " + $tableBodyj.children("td").eq(1).text() + "成功！");
-			},errorMethod);
+			function(){},errorMethod);
 	
 	$tableBodyj.fadeOut(800);
+}
+
+//保存点击修改键前的用户信息
+var tempUser = {};
+function changeUserFromTable(self){
+	
+	var row = self.parentNode;
+	var tableBody = row.parentNode;
+	$tableBodyj = $(tableBody);
+	//用户实名变成input
+	$realNameTd = $tableBodyj.children("td").eq(2);
+	tempUser.realName = $realNameTd.text();
+	$realNameTd.html("<input type=\"text\" value=\"" + $realNameTd.text() + "\" style=\"width=50px;heigt=50px\"/>");
+	
+	// 性别变成下拉框
+	$genderTd = $tableBodyj.children("td").eq(3);
+	tempUser.gender = $genderTd.text();
+	$genderTd.html("<select><option>" + $genderTd.text() +
+			"</option><option>" + ($genderTd.text()==='男' ? '女' : '男') + "</option></select>");
+	
+	//模型选择
+	// TODO
+	
+	//是否为管理员变为下拉框
+	$isAdminTd = $tableBodyj.children("td").eq(5);
+	tempUser.permission=$isAdminTd.text();
+	$isAdminTd.html("<select><option>" + $isAdminTd.text() +
+			"</option><option>" + ($isAdminTd.text()==='管理员' ? '普通用户' : '管理员') + "</option></select>");
+	
+	//修改按钮变为保存
+	$changeButton = $tableBodyj.children("td").eq(6).children().eq(0);
+	$changeButton.attr("onclick", "saveChange(this)")
+	$changeButton.text("保存");
+	
+	//删除按钮变为取消
+	$deleteButton = $tableBodyj.children("td").eq(6).children().eq(1);
+	$deleteButton.attr("onclick", "cancelChange(this, null)")
+	$deleteButton.text("取消");
+}
+
+function saveChange(self) {
+	var user = {};
+	
+	var row = self.parentNode;
+	var tableBody = row.parentNode;
+	$tableBodyj = $(tableBody);
+	// 获取用户实名
+	$realNameTd = $tableBodyj.children("td").eq(2);
+	user.realName = $realNameTd.children("input").eq(0).val();
+	
+	// 性别变成下拉框
+	$genderTd = $tableBodyj.children("td").eq(3);
+	user.gender = $genderTd.children("select").eq(0).find("option:selected").text();
+	
+	//模型选择
+	// TODO
+	
+	//是否为管理员变为下拉框
+	$isAdminTd = $tableBodyj.children("td").eq(5);
+	user.permission = $isAdminTd.children("select").eq(0).find("option:selected").text();
+	
+	//点击保存后传入修改后的数据再还原
+	cancelChange(self, user)
+}
+
+function cancelChange(self, user) {
+	
+	//如果数据是修改后的就把修改后的数据写上去！
+	if(user != null){
+		tempUser = user;
+	}
+	
+	var row = self.parentNode;
+	var tableBody = row.parentNode;
+	$tableBodyj = $(tableBody);//js对象转化为jq对象
+	
+	//用户实名还原
+	$realNameTd = $tableBodyj.children("td").eq(2);
+	$realNameTd.html(tempUser.realName);
+	
+	// 性别还原
+	$genderTd = $tableBodyj.children("td").eq(3);
+	$genderTd.html(tempUser.gender);
+	
+	//模型选择
+	// TODO
+	
+	//用户身份还原
+	$isAdminTd = $tableBodyj.children("td").eq(5);
+	$isAdminTd.html(tempUser.permission);
+	
+	//修改按钮变为保存
+	$changeButton = $tableBodyj.children("td").eq(6).children().eq(0);
+	$changeButton.attr("onclick", "changeUserFromTable(this)")
+	$changeButton.text("修改");
+	
+	//删除按钮变为取消
+	$deleteButton = $tableBodyj.children("td").eq(6).children().eq(1);
+	$deleteButton.attr("onclick", "deleteUserFromTable(this)")
+	$deleteButton.text("删除");
 }
 
 function ListUsers(result){
@@ -107,7 +205,7 @@ function ListUsers(result){
 				+ "</td><td>" + (obj.gender === true ? '男' : '女')
 				+ "</td><td>" + obj.model
 				+ "</td><td>" + (obj.permission === true ? '管理员' : "普通用户")
-				+ "</td><td>" + "<button class=\"operate_button\" onclick=\" \">修改</button>" 
+				+ "</td><td>" + "<button class=\"operate_button\" onclick=\"changeUserFromTable(this)\">修改</button>" 
 							  + "<button class=\"operate_button\" onclick=\"deleteUserFromTable(this) \">删除</button>"
 				+ "</td></tr>")
 	})
